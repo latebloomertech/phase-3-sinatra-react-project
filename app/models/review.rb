1f4c4abc84ada8c848f0e22d4e89a37164ad6377
Dan Baechtold <latebloomertech@gmail.com>
2022-03-10 10:30:08 -0500

    Code refinement

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
     belongs_to :movie
     belongs_to :user
 
-    # def print_review
-    #     puts "Review for #{self.movie.name} by #{self.user.name}: #{self.star_rating}. #{self.comment}"
-    # end
+    def print_review
+        "Review for #{self.movie.name} by #{self.user.name}: #{self.star_rating}. #{self.comment}"
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,28 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :movies, through: :reviews
 
-def favorite_product
+def favorite_movie
     self.reviews.all.order(:star_rating).last
 end
 
-# Not sure this is working correctly.  Tried a couple of different methods and ran out of time for testing
-# def remove_reviews(movie)
-#     self.reviews.where(movie_id: movie.id).destroy_all
-#     end
+def create_review(movie,rating,comment)
+    Review.create(star_rating: rating, comment: comment, user_id: self.id, movie_id: movie.id)
+end
+
+def all_user_reviews
+    self.reviews.each do |review|
+        "#{review.movie.name} rating: #{review.movie.star_rating}, review: #{review.movie.comment}"
+    end
+end
+
+def delete_review(movie)
+    review = self.reviews.find {|review| review.movie_id == movie.id}
+    review.destroy
+end
+
+def delete_all_reviews(movie)
+    review = self.movies.where(movie_id: movie.id)
+    review.destroy_all
+end
+
 end
